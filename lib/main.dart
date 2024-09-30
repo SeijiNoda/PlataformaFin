2b4fb8b9c7828ab4ad7b1531cff447921eaaffc4
Lima-Almeida <mateuzinho-7@hotmail.com>
2024-09-30 09:58:11 -0300

    <feat>(cadastro): att dependencias e main

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_app/firebase_options.dart';
 import 'package:my_app/pages/cadastro.dart';
-import 'pages/cadastroReceitas.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
